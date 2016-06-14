3f9480d109dce830914f8a17e747a82bcf609693
Johny Jean Michel <johnyjeanmichel@ufl.edu>
2016-06-14 16:32:53 -0400

    Extra files.

---
+++ StarForum.Infrastructure.Data/Repositories/ForumRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarForum.Infrastructure.Data.Repositories
+{
+    public class ForumRepository
+    {
+        public string[] GetLatestThreads()
+        {
+            return new string[] { "Need development help!", 
+                                  "Can someone help me with this?",
+                                  "JavaScript error!",
+                                  "Where can I start learning?",
+                                  "Looking for work..."};
+        }
+    }
+}
